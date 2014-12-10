8413db3cdd99d41cb256e38be0662fbe76399f86
Zifeng Xu <xuzifeng89@gmail.com>
2014-12-10 03:22:01 -0500

    The logic of try again button? It always starts the easy one.

---
+++ WebFianlProject/Scripts/objects/enemybullet.js
@@ -0,0 +1,21 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+// Bullet Class
+var objects;
+(function (objects) {
+    var Bullet_enemy = (function (_super) {
+        __extends(Bullet_enemy, _super);
+        function Bullet_enemy(game) {
+            _super.call(this, "bullet_down");
+            this.game = game;
+            this.game.addChild(this);
+        }
+        return Bullet_enemy;
+    })(objects.GameObject);
+    objects.Bullet_enemy = Bullet_enemy;
+})(objects || (objects = {}));
+//# sourceMappingURL=enemybullet.js.map
